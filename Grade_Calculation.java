0b37ca46a6ca8891b8c274a34dec9d01c389e6fa
Anubhav Tripathy <97749731+anubhav241@users.noreply.github.com>
2023-10-24 08:14:28 +0530

    Grade_Calculation.java

--- Grade_Calculation.java
+++ Grade_Calculation.java
@@ -11,23 +11,15 @@ public class Grade_Calculation {
 
         int[] marks = new int[size];
         for (int i = 0 ; i < marks.length ; i++){
-            System.out.print("Enter mark for sub["+(i+1)+"] : ");
+            System.out.print("Enter mark for sub["+(i+1)+"] out of 100: ");
             marks[i] = sc.nextInt();
         }
-
-        System.out.print("Full mark of individual subject: ");
-        int full_mark = sc.nextInt();
-        int full_marks = 0;
-        for (int i = 0; i<size ; i++){
-            full_marks += full_mark;
-        }
-//        System.out.println(full_marks);
 
         int total_marks = 0;
         for (int i = 0 ; i < marks.length ; i++){
             total_marks += marks[i];
         }
-        int percentage = (total_marks/full_marks)*100;
+        int percentage = total_marks/size;
 
         System.out.println("\nSo the total mark is : "+total_marks);
         System.out.println("The average percentage is : "+percentage+"%");
